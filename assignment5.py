76d79b03851ceda3a328148b4ebe5a5159d08996
DarkDuckComics <147108989+DarkDuckComics@users.noreply.github.com>
2023-12-12 20:35:17 -0500

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Function to load data from a CSV file
 def load_data(file_path):
     try:
         data = pd.read_csv(file_path)
@@ -12,7 +13,8 @@ def load_data(file_path):
         return None
 
 
-def analyze_population_data(data, column_name):
+# Function to analyze data and generate statistics and histograms
+def analyze_data(data, column_name, color='blue'):
     if column_name not in data.columns:
         print("Invalid column name. Please select a valid column.")
         return
@@ -31,41 +33,36 @@ def analyze_population_data(data, column_name):
     for stat, value in statistics.items():
         print(f"{stat} = {value}")
 
-    plt.hist(column_data, bins=20, color='blue', edgecolor='black')
+    plt.hist(column_data, bins=20, color=color, edgecolor='black')
     plt.xlabel(column_name)
     plt.ylabel('Frequency')
     plt.title(f'Histogram of {column_name}')
     plt.show()
 
 
-def analyze_housing_data(data, column_name):
-    if column_name not in data.columns:
-        print("Invalid column name. Please select a valid column.")
-        return
+def select_column(data, columns):
+    while True:
+        print("Select the Column you want to analyze:")
+        for idx, column in enumerate(columns):
+            print(f"{chr(ord('a') + idx)}. {column}")
 
-    column_data = data[column_name]
+        print(f"{chr(ord('a') + len(columns))}. Exit Column")
 
-    statistics = {
-        'Count': column_data.count(),
-        'Mean': column_data.mean(),
-        'Standard Deviation': column_data.std(),
-        'Min': column_data.min(),
-        'Max': column_data.max()
-    }
+        column_choice = input().lower()
 
-    print(f"The statistics for column {column_name} are:")
-    for stat, value in statistics.items():
-        print(f"{stat} = {value}")
-
-    plt.hist(column_data, bins=20, color='green', edgecolor='black')
-    plt.xlabel(column_name)
-    plt.ylabel('Frequency')
-    plt.title(f'Histogram of {column_name}')
-    plt.show()
+        if column_choice == chr(ord('a') + len(columns)):
+            print("You selected to exit the column menu")
+            break
+        elif column_choice in [chr(ord('a') + i) for i in range(len(columns))]:
+            selected_column = columns[ord(column_choice) - ord('a')]
+            analyze_data(data, selected_column)
+        else:
+            print("Invalid choice. Please enter a valid option.")
 
 
 def main():
     print("***************** Welcome to the Python Data Analysis App**********")
+
     while True:
         print("Select the file you want to analyze:")
         print("1. Population Data")
@@ -73,8 +70,9 @@ def main():
         print("3. Exit the Program")
 
         choice = input()
+
         if choice == '1':
-            population_data_path = r"C:\Users\xXste\Downloads\PopChange.csv"  # Replace with your actual file path
+            population_data_path = r"C:\Users\xXste\Downloads\PopChange.csv"  # Path to local file
             population_data = load_data(population_data_path)
 
             if population_data is not None:
@@ -87,17 +85,17 @@ def main():
 
                     column_choice = input()
                     if column_choice == 'a':
-                        analyze_population_data(population_data, 'Pop Apr 1')
+                        analyze_data(population_data, 'Pop Apr 1')
                     elif column_choice == 'b':
-                        analyze_population_data(population_data, 'Pop Jul 1')
+                        analyze_data(population_data, 'Pop Jul 1')
                     elif column_choice == 'c':
-                        analyze_population_data(population_data, 'Change Pop')
+                        analyze_data(population_data, 'Change Pop')
                     elif column_choice == 'd':
                         break
                     else:
                         print("Invalid choice. Please enter a valid option.")
         elif choice == '2':
-            housing_data_path = r"C:\Users\xXste\Downloads\Housing.csv"  # Replace with your actual file path
+            housing_data_path = r"C:\Users\xXste\Downloads\Housing.csv"  # Path to local file
             housing_data = load_data(housing_data_path)
 
             if housing_data is not None:
@@ -112,15 +110,15 @@ def main():
 
                     column_choice = input()
                     if column_choice == 'a':
-                        analyze_housing_data(housing_data, 'AGE')
+                        analyze_data(housing_data, 'AGE')
                     elif column_choice == 'b':
-                        analyze_housing_data(housing_data, 'BEDRMS')
+                        analyze_data(housing_data, 'BEDRMS')
                     elif column_choice == 'c':
-                        analyze_housing_data(housing_data, 'BUILT')
+                        analyze_data(housing_data, 'BUILT')
                     elif column_choice == 'd':
-                        analyze_housing_data(housing_data, 'ROOMS')
+                        analyze_data(housing_data, 'ROOMS')
                     elif column_choice == 'e':
-                        analyze_housing_data(housing_data, 'UTILITY')
+                        analyze_data(housing_data, 'UTILITY')
                     elif column_choice == 'f':
                         break
                     else:
